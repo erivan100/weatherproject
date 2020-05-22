3fd5517b4ad6dd934f4b24915e72c233776e66a2
erivan100 <erivan.so@hotmail.com>
2020-05-21 21:49:20 -0300

    request

--- login.js
+++ login.js
@@ -1,3 +1,6 @@
+localStorage.clear();
+
+
  $("#login-button").click(function(event){
      event.preventDefault();
     
@@ -11,11 +14,25 @@
 
      $('.wrapper > .container > h1').html("Seja bem-vindo de volta!");
      $('form').fadeOut(500, function(){
-         setTimeout(function(){
+         console.log(form.pass.value);
+        let jsonOBj = {email: form.user.value, password: form.pass.value};
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', "http://52.91.139.190/fsapi/users/login", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) { 
+               /*  if (xhr.status = 200) {
+                    
+                } */
+                console.log(xhr.responseText);
+            }
+        }
+
+        xhr.send(JSON.stringify(jsonOBj));
+/*          setTimeout(function(){
             localStorage.setItem('token',"logadoteste");
             window.location.href = "index.html";
          },2000);
-        
+ */        
      });
      $('.wrapper').addClass('form-success');
      

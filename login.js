2371e275dab3d9ed7025afbb43f6ceac5ccd4ccf
erivan100 <erivan.so@hotmail.com>
2020-05-21 22:35:36 -0300

    Request atualizacão

--- login.js
+++ login.js
@@ -12,55 +12,40 @@ $("#login-button").click(function (event) {
         return;
     }
 
-    $('.wrapper > .container > h1').html("Seja bem-vindo de volta!");
 
-    
+
+
     let jsonOBj = { email: form.user.value, password: form.pass.value };
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://52.91.139.190/fsapi/users/login", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-  /*   xhr.setRequestHeader("Authorization", "bearer 312421312412312312412312312"); */
-    
+    /*   xhr.setRequestHeader("Authorization", "bearer 312421312412312312412312312"); */
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            if(xhr.status == 200){
-                console.log(xhr.responseText);
+            if (xhr.status == 200) {
+                let response = JSON.parse(xhr.responseText);
+                localStorage.setItem("token", response.token);
+                // console.log(xhr.responseText);
+                $('.wrapper > .container > h1').html("Seja bem-vindo de volta!");
+                $('form').fadeOut(500, function () {
+
+                    setTimeout(function () {
+                        window.location.href = "index.html";
+                    }, 2000);
+
+                });
+                $('.wrapper').addClass('form-success');
             }
-            else{
-                
+            else {
+                alert("O usuario não existe");
             }
-            
-                
-           
-        }   
+        }
     }
-    
-
     xhr.send(JSON.stringify(jsonOBj));
-
-
-
-
-
-
-
-
-
-
-
 });
 
 
 
 
 
-/* $('form').fadeOut(500, function(){
-    console.log(form.pass.value);
-
-    setTimeout(function(){
-       localStorage.setItem('token',"logadoteste");
-       window.location.href = "index.html";
-    },2000);
-
-});
-$('.wrapper').addClass('form-success'); */
